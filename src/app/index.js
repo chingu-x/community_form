a302bffce44154f7b917702df61c5517d62b9dd1
the-vampiire <vampiirecodes@gmail.com>
2018-04-18 15:24:02 -0400

    Base build of Button, Question, and Form components
    - tested and working to render from the questions array

--- src/app/index.js
+++ src/app/index.js
@@ -1,46 +1,125 @@
 import React, { Component } from 'react';
 import * as ReactDOM from 'react-dom';
-const axios = require('axios');
+import * as questions from './questions';
 
-class Form extends Component {
+
+class Button extends Component {
+    render() {
+        const { action, disabled, text } = this.props;
+        return (
+            <button onClick={action} disabled={disabled}>
+                {text}
+            </button>
+        );
+    }
+}
+
+class Question extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            fields: []
+            input: ''
         }
     }
 
-    getFormFields () {
-        return axios.get('http://localhost:3000/form')
-            .then(({ data }) => data.fields)
-    };
+    // on text input method (updates state.input)
+    
+    // on select method (updates state.input)
 
-    componentDidMount() {
-        this.getFormFields()
-        .then(fields => this.setState(() => ({ fields })))
-        .catch(console.error);
+    textInput() {
+        const { name, description, type, required } = this.props.question;
+        console.log(this.props)
+        return (
+            <div>
+                <label>{description}</label>
+                <input type={type || "text"} name={name} required={required} />
+            </div>
+        );
+    }
+
+    selectInput() {
+        const { name, description, options, required } = this.props.question;
+        return (
+            <div>
+                <label>{description}</label>
+                <select name={name} required={required}> {
+                    options.map((option, i) => (
+                        <option value={option} key={i}>
+                            {option}
+                        </option>
+                    ))
+                }
+                </select>
+            </div>
+        );
     }
 
     render() {
         return (
             <div>
-                <form method="POST" action="/form/"> {
-                   this.state.fields.map(
-                       ({ description, name, required, type, options }) => {
-                           
-                           return (
-                               <div key={name}>
-                                   <label>{description}</label>
-                                   <input name={name} type={type} />
-                                   { options ? options.map(option => (<option value={option}>{option}</option>)) : null }
-                               </div>
-                           );
-                       }
-                   ) 
-                } </form>
+                { 
+                    this.props.question.type === 'select' 
+                       ? this.selectInput() 
+                       : this.textInput() 
+                }
+            </div>
+        )
+    }
+ }
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            previous: null, // index of previous question in questions arr
+            next: 0, // index of next question in questions arr
+            responses: {}, // stores responses
+            nextDisabled: true,
+        }
+
+        this.onNext = this.onNext.bind(this);
+        this.onPrevious = this.onPrevious.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onNext() {
+        // incremenet the state 'next'
+        // previous = next++
+    }
+
+    onPrevious() {
+
+    }
+
+    onSubmit() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <Question question={this.props.questions[this.state.next]} />
+                { 
+                    this.state.previous
+                        ? <Button action={this.onPrevious} text='Previous' />
+                        : null
+                }
+                <Button action={this.onNext} text='Next' disabled={false} />
             </div>
         );
     }
 }
 
-ReactDOM.render(<Form />, document.querySelector('#app'));
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Form questions={questions} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
